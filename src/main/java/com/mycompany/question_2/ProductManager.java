24d4a76087d59d131f708179fb24a9ed1aec7128
tarun <parmatar@sheridancollege.ca>
2026-02-18 14:26:25 -0500

    Initial commit by Tarun Parmar

---
+++ src/main/java/com/mycompany/question_2/ProductManager.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.question_2;
+
+/**
+ *
+ * @author tarun
+ */
+public class ProductManager {
+
+    public ProductManager() {
+        System.out.println("ProductManager initialized");
+    }
+}
